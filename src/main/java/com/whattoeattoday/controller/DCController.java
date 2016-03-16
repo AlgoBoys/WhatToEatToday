0adb5133d0c38350c4e4dc79a6a11f2d05085564
ravi_k_soni <ravikantsoni03@gmail.com>
2016-03-16 09:59:19 +0530

    .

--- src/main/java/com/whattoeattoday/controller/DCController.java
+++ src/main/java/com/whattoeattoday/controller/DCController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/foodies")
 public class DCController {
 
 	@RequestMapping("/dclist")
-	public void moo() {
+	public String moo() {
 		System.out.println("---------------");
+		return "dclist";
 	}
 
 }
